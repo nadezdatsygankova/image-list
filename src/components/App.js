ceb0a7e5ff9d300fbb844f56edb94b3866a228c1
nadezdatsygankova <nadiapritchina@gamil.com>
2022-02-17 22:26:53 -0500

    new project

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SearchInput from './SearchInput';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    }
+
+
+    onSearchSubmit(entry) {
+        console.log(entry)
+    }
+
+
+
+    render() {
+        return (
+            <div className='ui container ' style={{ marginTop: '30px' }} >
+                <SearchInput onSearchSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
+
+}
+
+
+export default App;
