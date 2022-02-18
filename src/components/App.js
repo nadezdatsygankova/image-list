92ed6f8e32d7f213572b44c65c84912b679d824a
nadezdatsygankova <nadiapritchina@gamil.com>
2022-02-17 23:03:37 -0500

    add async

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchInput from './SearchInput';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props) {
@@ -8,8 +9,10 @@ class App extends React.Component {
     }
 
 
-    onSearchSubmit(entry) {
-        console.log(entry)
+    async onSearchSubmit(entry) {
+        const response = await
+            axios.get(`https://pixabay.com/api/?key=25762307-564ff7be8a1412aa385951d21&q=${entry}&image_type=photo`);
+        console.log(response);
     }
 
 
