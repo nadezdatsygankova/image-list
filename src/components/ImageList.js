1632b670eb5889c3bab3716b8567d655bf321500
nadezdatsygankova <nadiapritchina@gamil.com>
2022-02-21 21:47:14 -0500

    add css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./image.css";
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
-        return <img key={image.id} src={image.webformatURL} alt="image" />
+        return <img className="imagesInside" key={image.id} src={image.webformatURL} alt="image" />
     })
     return (
-        <div>
+        <div className="images">
             {images}
         </div>
     )
